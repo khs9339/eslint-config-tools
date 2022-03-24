8d3f61215a42be3abb353453c5389ae9b43a290d
김효성 <hyo@wemakeprice.com>
2022-03-24 21:11:48 +0900

    eslint extends 버그 수정

--- index.js
+++ index.js
@@ -1,34 +1,30 @@
 module.exports = {
-    "root": true,
-    "env": {
-        "es2021": true,
-        "browser": true,
-        "node": true
-    },
-    "extends": [
-        "eslint:recommended",
-        ['./rules/eslint', './rules/tslint'].map((e) => require.resolve(e)),
-    ],
-    "plugins": [],
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module",
-    },
-    "ignorePatterns": [
-        "/coverage",
-        "/bin",
-        "/dist",
-        "/build",
-        "/node_modules",
-        "/out",
-        "/public",
-        "/static",
-        "/.coverage",
-        "/.git",
-        "/.idea",
-        "/.vscode",
-        "/src/assets",
-        "*.config.js"
-    ],
+  root: true,
+  env: {
+    es2021: true,
+    browser: true,
+    node: true,
+  },
+  extends: ['./rules/eslint', './rules/tslint'].map((e)=> require.resolve(e)),
+  plugins: [],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  ignorePatterns: [
+    '/coverage',
+    '/bin',
+    '/dist',
+    '/build',
+    '/node_modules',
+    '/out',
+    '/public',
+    '/static',
+    '/.coverage',
+    '/.git',
+    '/.idea',
+    '/.vscode',
+    '/src/assets',
+    '*.config.js',
+  ],
 };
-
--- rules/eslint.js
+++ rules/eslint.js
@@ -1,185 +1,186 @@
 module.exports = {
-    rules: {
-        "indent": [
-            "error",
-            2,
-            {
-                "SwitchCase": 1,
-                "VariableDeclarator": 1,
-                "outerIIFEBody": 1,
-                "MemberExpression": 1,
-                "FunctionDeclaration": {
-                    "parameters": 1,
-                    "body": 1
-                },
-                "FunctionExpression": {
-                    "parameters": 1,
-                    "body": 1
-                },
-                "CallExpression": {
-                    "arguments": 1
-                },
-                "ArrayExpression": 1,
-                "ObjectExpression": 1,
-                "ImportDeclaration": 1,
-                "flatTernaryExpressions": false,
-                "ignoreComments": false,
-                "ignoredNodes": [
-                    "TemplateLiteral *"
-                ],
-                "offsetTernaryExpressions": false
-            }
+  extends: 'eslint:recommended',
+  rules: {
+    'indent': [
+      'error',
+      2,
+      {
+        'SwitchCase': 1,
+        'VariableDeclarator': 1,
+        'outerIIFEBody': 1,
+        'MemberExpression': 1,
+        'FunctionDeclaration': {
+          'parameters': 1,
+          'body': 1,
+        },
+        'FunctionExpression': {
+          'parameters': 1,
+          'body': 1,
+        },
+        'CallExpression': {
+          'arguments': 1,
+        },
+        'ArrayExpression': 1,
+        'ObjectExpression': 1,
+        'ImportDeclaration': 1,
+        'flatTernaryExpressions': false,
+        'ignoreComments': false,
+        'ignoredNodes': [
+          'TemplateLiteral *',
         ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "semi-spacing": [
-            "error",
-            {
-                "before": false,
-                "after": true
-            }
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "comma-dangle": [
-            "error",
-            {
-                "arrays": "always-multiline",
-                "objects": "always-multiline",
-                "imports": "always-multiline",
-                "exports": "never",
-                "functions": "never"
-            }
-        ],
-        "no-unused-vars": [
-            "warn",
-            {
-                "vars": "all",
-                "args": "none",
-                "ignoreRestSiblings": true
-            }
-        ],
-        "array-bracket-spacing": [
-            "error",
-            "never"
-        ],
-        "arrow-spacing": [
-            "error",
-            {
-                "before": false,
-                "after": true
-            }
-        ],
-        "block-spacing": [
-            "error",
-            "always"
-        ],
-        "brace-style": [
-            "error",
-            "1tbs",
-            {
-                "allowSingleLine": true
-            }
-        ],
-        "camelcase": [
-            "error",
-            {
-                "properties": "never",
-                "ignoreDestructuring": false,
-                "ignoreImports": false,
-                "ignoreGlobals": false
-            }
-        ],
-        "comma-spacing": [
-            "error",
-            {
-                "before": false,
-                "after": true
-            }
-        ],
-        "computed-property-spacing": [
-            "error",
-            "never"
-        ],
-        "curly": [
-            "error",
-            "multi-line"
-        ],
-        "eqeqeq": [
-            "error",
-            "always",
-            {
-                "null": "ignore"
-            }
-        ],
-        "func-call-spacing": [
-            "error",
-            "never"
-        ],
-        "handle-callback-err": [
-            "error",
-            "^(err|error)$"
-        ],
-        "key-spacing": [
-            "error",
-            {
-                "beforeColon": false,
-                "afterColon": true
-            }
-        ],
-        "keyword-spacing": [
-            "error",
-            {
-                "before": true,
-                "after": true
-            }
-        ],
-        "no-multiple-empty-lines": [
-            "error",
-            {
-                "max": 1,
-                "maxEOF": 0
-            }
-        ],
-        "space-in-parens": [
-            "error",
-            "never"
-        ],
-        "space-infix-ops": [
-            "error"
-        ],
-        "no-dupe-args": [
-            "error"
-        ],
-        "no-dupe-class-members": [
-            "error"
-        ],
-        "no-dupe-keys": [
-            "error"
-        ],
-        "no-duplicate-case": [
-            "error"
-        ],
-        "no-func-assign": [
-            "error"
-        ],
-        "no-global-assign": [
-            "error"
-        ],
-        "object-curly-spacing": [
-            "error",
-            "always"
-        ],
-        "object-property-newline": [
-            "error",
-            {
-                "allowMultiplePropertiesPerLine": true,
-                "allowAllPropertiesOnSameLine": false
-            }
-        ],
-    }
+        'offsetTernaryExpressions': false,
+      },
+    ],
+    'semi': [
+      'error',
+      'always',
+    ],
+    'semi-spacing': [
+      'error',
+      {
+        'before': false,
+        'after': true,
+      },
+    ],
+    'quotes': [
+      'error',
+      'single',
+    ],
+    'comma-dangle': [
+      'error',
+      {
+        'arrays': 'always-multiline',
+        'objects': 'always-multiline',
+        'imports': 'always-multiline',
+        'exports': 'never',
+        'functions': 'never',
+      },
+    ],
+    'no-unused-vars': [
+      'warn',
+      {
+        'vars': 'all',
+        'args': 'none',
+        'ignoreRestSiblings': true,
+      },
+    ],
+    'array-bracket-spacing': [
+      'error',
+      'never',
+    ],
+    'arrow-spacing': [
+      'error',
+      {
+        'before': false,
+        'after': true,
+      },
+    ],
+    'block-spacing': [
+      'error',
+      'always',
+    ],
+    'brace-style': [
+      'error',
+      '1tbs',
+      {
+        'allowSingleLine': true,
+      },
+    ],
+    'camelcase': [
+      'error',
+      {
+        'properties': 'never',
+        'ignoreDestructuring': false,
+        'ignoreImports': false,
+        'ignoreGlobals': false,
+      },
+    ],
+    'comma-spacing': [
+      'error',
+      {
+        'before': false,
+        'after': true,
+      },
+    ],
+    'computed-property-spacing': [
+      'error',
+      'never',
+    ],
+    'curly': [
+      'error',
+      'multi-line',
+    ],
+    'eqeqeq': [
+      'error',
+      'always',
+      {
+        'null': 'ignore',
+      },
+    ],
+    'func-call-spacing': [
+      'error',
+      'never',
+    ],
+    'handle-callback-err': [
+      'error',
+      '^(err|error)$',
+    ],
+    'key-spacing': [
+      'error',
+      {
+        'beforeColon': false,
+        'afterColon': true,
+      },
+    ],
+    'keyword-spacing': [
+      'error',
+      {
+        'before': true,
+        'after': true,
+      },
+    ],
+    'no-multiple-empty-lines': [
+      'error',
+      {
+        'max': 1,
+        'maxEOF': 0,
+      },
+    ],
+    'space-in-parens': [
+      'error',
+      'never',
+    ],
+    'space-infix-ops': [
+      'error',
+    ],
+    'no-dupe-args': [
+      'error',
+    ],
+    'no-dupe-class-members': [
+      'error',
+    ],
+    'no-dupe-keys': [
+      'error',
+    ],
+    'no-duplicate-case': [
+      'error',
+    ],
+    'no-func-assign': [
+      'error',
+    ],
+    'no-global-assign': [
+      'error',
+    ],
+    'object-curly-spacing': [
+      'error',
+      'always',
+    ],
+    'object-property-newline': [
+      'error',
+      {
+        'allowMultiplePropertiesPerLine': true,
+        'allowAllPropertiesOnSameLine': false,
+      },
+    ],
+  },
 };
