9dcb4660ba2ea8218c52a51170a26d776a95d4e8
김효성 <hyo@wemakeprice.com>
2022-03-24 12:25:50 +0900

    eslint init

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+    // 확장할 설정
+    "extends": [ "eslint:recommended" ],
+    // 플러그인 추가
+    "plugins": [ "import" ],
+    // 사용할 규칙 설정
+    "rules": {
+        "semi": "error",
+        "no-console": "error",
+    },
+  }
