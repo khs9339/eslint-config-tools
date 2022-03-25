5a2e93148cfedeed953905aa5fe61745e0767396
김효성 <hyo@wemakeprice.com>
2022-03-25 14:34:32 +0900

    typescript rule 추가

---
+++ typescript.js
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  env: {
+    es2021: true,
+    browser: true,
+    node: true,
+  },
+  extends: ['./rules/eslint', './rules/tslint'].map((e)=> require.resolve(e)),
+  plugins: [],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  ignorePatterns: [
+    '/coverage',
+    '/bin',
+    '/dist',
+    '/build',
+    '/node_modules',
+    '/out',
+    '/public',
+    '/static',
+    '/.coverage',
+    '/.git',
+    '/.idea',
+    '/.vscode',
+    '/src/assets',
+    '*.config.js',
+  ],
+};
