78c76694308e0e727b9883e538bb5479c6d90137
김효성 <hyo@wemakeprice.com>
2022-03-29 15:59:19 +0900

    build script

--- script/prepublish.js
+++ script/prepublish.js
@@ -1,3 +1,5 @@
 const { execSync } = require('child_process');
 var version = execSync('npm version patch').toString().trim();
 console.log(`Next Version: ${version}`);
+
+execSync(`npm dist-tag add eslint-config-tools@${version.replace('v', '')} dev`);
