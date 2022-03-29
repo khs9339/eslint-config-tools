5f3443527561dfe8a5446df069adb044a154ddb1
김효성 <hyo@wemakeprice.com>
2022-03-29 16:03:44 +0900

    script 수정

--- script/postpublish.js
+++ script/postpublish.js
@@ -1,3 +1,5 @@
 const { execSync } = require('child_process');
 const { version } = require('../package.json');
+
+console.log('::: DIST TAG MOVE');
 execSync(`npm dist-tag add eslint-config-tools@${version} dev`);
